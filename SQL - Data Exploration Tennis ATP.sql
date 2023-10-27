e40f1514e3456144602b22ddd9185c5efff676e6
MarinKabic <131856660+MarinKabic@users.noreply.github.com>
2023-10-27 13:24:55 +0200

    Update SQL - Data Exploration Tennis ATP.sql

--- SQL - Data Exploration Tennis ATP.sql
+++ SQL - Data Exploration Tennis ATP.sql
@@ -9,7 +9,6 @@ and we will join First_Name and Last_Name into one column named 'Player_Name'
 ------Creating a table where data from both tables will be joined----------
 ---------------------------------------------------------------------------
 
-
 CREATE TABLE ATP_Ranks
 (
 Ranking_date date,
@@ -27,11 +26,11 @@ INSERT INTO ATP_Ranks
 SELECT r.ranking_date,
        r.rank,
        CONCAT(p.name_first, ' ', p.name_last) AS full_name,
-	   r.points,
-	   p.hand,
-	   p.dob,
-	   p.ioc,
-	   p.height,
+       r.points,
+       p.hand,
+       p.dob,
+       p.ioc,
+       p.height,
        p.player_id 
 FROM atp_players AS p
 JOIN atp_rankings_10s AS r
@@ -42,6 +41,8 @@ ORDER BY r.ranking_date
 SELECT *
 FROM ATP_Ranks
 
+
+	
 ---------------------------------------------------------------------------
 ----------List all the names of no. 1. players during the period-----------
 ---------------------------------------------------------------------------
@@ -51,8 +52,7 @@ FROM ATP_Ranks
 WHERE Player_Rank = 1
 GROUP BY Player_Name
 
-SELECT TOP 5 *
-FROM ATP_Ranks
+
 
 ----------------------------------------------------------------------------
 --------------------------------Days observed-------------------------------
@@ -65,12 +65,13 @@ FROM ATP_Ranks
 -------------------Who has spent the most days at rank 1?-------------------
 ----------------------------------------------------------------------------
 
-WITH RankData AS (
+WITH RankData AS 
+(
     SELECT
         Player_Name,
         Ranking_date,
         Player_Rank,
-		DATEDIFF(DAY, Ranking_date, LEAD(Ranking_date) OVER(ORDER BY Ranking_date)) AS days_diff
+	DATEDIFF(DAY, Ranking_date, LEAD(Ranking_date) OVER(ORDER BY Ranking_date)) AS days_diff
     FROM ATP_Ranks
     WHERE Player_Rank = 1
 )
@@ -92,21 +93,23 @@ ORDER BY SUM(days_diff) DESC;
 ----------------------------------------------------------------------------
 
 SELECT Player_Rank,
-	Player_Name,
-    MAX(Points) AS max_points,
-    MIN(Points) AS min_points
+       Player_Name,
+       MAX(Points) AS max_points,
+       MIN(Points) AS min_points
 FROM ATP_Ranks
 WHERE Player_Rank = 1 
 GROUP BY Player_Rank,
-	     Player_Name
+         Player_Name
 
+
+	
 ----------------------------------------------------------------------------
 -------------------AVG yearly rank of each no.1 player----------------------
 ----------------------------------------------------------------------------
 
 SELECT Player_Name,
        YEAR(Ranking_date) AS Ranking_Year,
-	   AVG(Player_Rank) AS Average_Yearly_Rank
+       AVG(Player_Rank) AS Average_Yearly_Rank
 FROM ATP_Ranks
 WHERE Player_Name IN ('Novak Djokovic', 'Roger Federer', 'Rafael Nadal', 'Andy Murray')
 GROUP BY Player_Name,
@@ -141,12 +144,13 @@ SELECT
 	Player_Name
 FROM ATP_Ranks
 WHERE Player_Name NOT IN ('Novak Djokovic', 'Rafael Nadal', 'Roger Federer', 'Andy Murray')
-	AND Player_Rank <= 3
+      AND Player_Rank <= 3
 GROUP BY 
 	Player_Rank,
 	Player_Name
 
 
+	
 ---------------------------------------------------------------------------
 --top 10 players with the highest average ranking over the entire period?--
 ---------------------------------------------------------------------------
@@ -156,8 +160,8 @@ GROUP BY
 WITH Top10 AS
 (
 	SELECT Player_Name,
-		AVG(Player_Rank) AS Average_Rank,
-		DENSE_RANK() OVER(ORDER BY AVG(Player_Rank)) AS cte_rank
+	       AVG(Player_Rank) AS Average_Rank,
+	       DENSE_RANK() OVER(ORDER BY AVG(Player_Rank)) AS cte_rank
 	FROM ATP_Ranks
 	GROUP BY Player_Name
 	HAVING COUNT(DISTINCT YEAR(Ranking_date)) = 10
@@ -167,6 +171,7 @@ SELECT Player_Name,
 FROM Top10 
 WHERE cte_rank <= 10
 
+	
 
 ---------------------------------------------------------------------------
 ----------Show players which had the average ranking inside TOP 10---------
@@ -180,33 +185,30 @@ HAVING AVG(Player_Rank) < 10
 
 
 
-
 ----------------------------------------------------------------------------
 --------------------------Ranking dates analysis----------------------------
 ----------------------------------------------------------------------------
 
 -- converting ranking_date into desired DATE format
-
+	
 UPDATE atp_rankings_10s
 SET ranking_date = FORMAT(CAST(ranking_date AS date), 'yyyy-MM-dd')
 -- atp_rankings_10s table wont be used, therefore the above query is for demonstration purposes only
 
-
-
 -- displaying the first and last ranking update
-
+	
 SELECT MIN(Ranking_date) AS first_ranking_date,
-	   MAX(Ranking_date) AS last_ranking_date
+       MAX(Ranking_date) AS last_ranking_date
 FROM ATP_Ranks
 
 
+	
 -- ATP rankings are cosindered to be weekly rankings due to them being updated weekly (at the end of the week) when the tournaments end
 -- the only exceptions are when there are no tournamets during the week
 -- the below analysis will display:
 -- 1. the count of rank updates, 
 -- 2. the number of weeks observed
 -- 3. determine what were the dates when ATP rankings weren't updated weekly?
-
 
 ----------------------------------------------------------------------------
 --------------------------Count of rank updates-----------------------------
@@ -235,7 +237,7 @@ FROM ATP_Ranks
 WITH CTE AS 
 (
     SELECT ROW_NUMBER() OVER (ORDER BY Ranking_date) AS row_numb, 
-	       ranking_date
+	   ranking_date
     FROM ATP_Ranks
 )
 SELECT CTE1.ranking_date AS currentdate, 
@@ -256,7 +258,7 @@ CREATE TABLE #temp_table
 
 INSERT INTO #temp_table
     SELECT ROW_NUMBER() OVER (ORDER BY Ranking_date) AS row_numb, 
-	       Ranking_date
+	   Ranking_date
     FROM ATP_Ranks
 
 SELECT t1.ranking_date AS currentdate, 
@@ -291,9 +293,9 @@ WITH CTE AS
 )
 SELECT Player_Rank,
        Player_Name,
-	   Ranking_date,
-	   prev_rank,
-	   prev_rank_date
+       Ranking_date,
+       prev_rank,
+       prev_rank_date
 FROM CTE
 WHERE Player_Rank = 1
 AND prev_rank <> 1
@@ -308,12 +310,13 @@ ORDER BY Ranking_date
 SELECT 
 	Player_Rank,
 	Player_Name,
-	SUM(CASE 
+	SUM(
+	     CASE 
 		WHEN Player_Rank <= 3 
 		THEN 1
 		ELSE 0
 		END
-		) AS total_weeks_in_top3
+	    ) AS total_weeks_in_top3
 FROM ATP_Ranks
 WHERE Player_Name NOT IN ('Novak Djokovic', 'Roger Federer', 'Andy Murray', 'Rafael Nadal')
 AND Player_Rank <= 3 
@@ -323,6 +326,7 @@ GROUP BY
 ORDER BY total_weeks_in_top3 DESC
 
 
+	
 ----------------------------------------------------------------------------
 --------How many weeks did any player spend at any given position?----------
 ----------------------------------------------------------------------------
@@ -331,12 +335,13 @@ ORDER BY total_weeks_in_top3 DESC
 
 SELECT 
 	Player_Name,
-	SUM(CASE 
+	SUM(
+	     CASE 
 		WHEN Player_Rank = 4
 		THEN 1
 		ELSE 0
 		END
-		) AS weekly_rank_4th
+	    ) AS weekly_rank_4th
 FROM ATP_Ranks
 WHERE Player_Name = 'Novak Djokovic' 
 AND Player_Rank = 4
@@ -380,18 +385,17 @@ FROM ATP_Ranks
 WHERE
     ranking_date = '2010-01-04'
 
+	
 
 ----------------------------------------------------
 ------------------CREATING VIEWS--------------------
 ----------------------------------------------------
 
 CREATE VIEW No1_ranked_players AS
-SELECT 
-	Player_Name
+SELECT Player_Name
 FROM ATP_Ranks
 WHERE Player_Rank = 1
-GROUP BY 	
-	Player_Name
+GROUP BY Player_Name
 
 
 CREATE VIEW FirstAndLast_no1_date AS
